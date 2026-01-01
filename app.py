ebdbeb94775b274d11475dabd1a09a64c66ff87a
souvik patra <souvikpatra066@gmail.com>
2026-01-01 19:00:33 +0530

    Fix deployment: Added gunicorn, app.py, and production config

---
+++ app.py
@@ -0,0 +1,4 @@
+from youtube_video_downloder import app
+
+if __name__ == '__main__':
+    app.run()
--- youtube_video_downloder.py
+++ youtube_video_downloder.py
@@ -271,5 +271,4 @@ def get_status(download_id):
 
 
 if __name__ == '__main__':
-    # debug True sirf development ke liye
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))00)
